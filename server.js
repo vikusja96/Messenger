a857435147cd69ce32b5ac5a8cb8219a4ab6c2ae
vikusja96 <vikusja96@gmail.com>
2021-10-11 22:17:02 +0100

    replase hardcoded user to actual username

--- server.js
+++ server.js
@@ -1,26 +1,37 @@
-const path = require('path');
-const express = require('express');
-const http = require('http');
+const path = require("path");
+const express = require("express");
+const http = require("http");
 const socketio = require("socket.io");
-const {formatMessage} = require('./utils/messages')
+const { formatMessage } = require("./utils/messages");
+const { userJoin, getCurrentUser } = require("./utils/users");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")));
 
-io.on('connection', (socket) => { 
-  socket.emit('message', formatMessage('admin', 'Welcome to Chat'));  
+io.on("connection", (socket) => {
+  socket.on("joinRoom", ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+    socket.join(user.room);
 
-  socket.broadcast.emit('message', formatMessage('admin','a user has connected to Chat'));
-
-  socket.on('chatMessage', (msg) => {
-    io.emit('message', formatMessage('user', msg));
+    socket.emit("message", formatMessage("admin", "Welcome to Chat"));
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        formatMessage("admin", `${user.username} has joined to Chat`)
+      );
   });
 
-  socket.on('disconnect', () => {
-    io.emit('message', formatMessage('admin','A user has left the chat'));
+  socket.on("chatMessage", (msg) => {
+    const user = getCurrentUser(socket.io);
+    io.to(user.room).emit("message", formatMessage(user.username, msg));
+  });
+
+  socket.on("disconnect", () => {
+    io.emit("message", formatMessage("admin", "A user has left the chat"));
   });
 });
 
