ea6c313c04f9ddcb08354104410e210c668a1c2a
vikusja96 <vikusja96@gmail.com>
2021-10-12 10:42:15 +0100

    add function that display users in the room

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,7 @@
 const chatForm = document.getElementById('chat-form');
 const chatMessages = document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
 
 const {username, room } = Qs.parse(location.search, {
   ignoreQueryPrefix: true
@@ -8,6 +10,11 @@ const {username, room } = Qs.parse(location.search, {
 const socket = io();
 
 socket.emit('joinRoom', {username, room});
+
+socket.on('roomUsers', ({room, users}) => {
+  outputRoomName(room);
+  outputUser(users);
+})
 
 socket.on('message', message => {
   console.log(message);
@@ -34,4 +41,13 @@ const outputMessage = (message) => {
   ${message.text}
   </p>`;
   document.querySelector('.chat-messages').appendChild(div);
+}
+
+const outputRoomName = (room) => {
+  roomName.innerText = room;
+}
+
+const outputUser = (users) => {
+  userList.innerHTML = `
+  ${users.map(user => `<li>${user.username}</li>`).join('')}`
 }
