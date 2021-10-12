c1957956f01488dd0e0d572fa05a24c9107556f1
vikusja96 <vikusja96@gmail.com>
2021-10-12 09:36:13 +0100

    create functions that handle user disconection

--- utils/users.js
+++ utils/users.js
@@ -9,3 +9,15 @@ exports.userJoin = (id, username, room) => {
 exports.getCurrentUser = () => {
   return users.find(user => user.id = id);
 };
+
+exports.userLeave = (id) => {
+  const index = users.findIndex(user => user.id === id);
+
+  if(index !== -1) {
+    return users.splice(index, 1)[0];
+  }
+};
+
+exports.getRoomUsers = (room) => {
+  return users.filter(user => user.room === room);
+};
